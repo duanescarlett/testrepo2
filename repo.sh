ce6d999d08181b3c53a162b89aa31a54ae27950e
D.Dos <duanescarlett@gmail.com>
2021-01-05 07:41:47 -0500

    Init Commit

--- repo.sh
+++ repo.sh
@@ -1,16 +1,9 @@
 !#/bin/bash
 
-repo=''
-
-while getopts :r options; do
-	case $flag in
-		r) repo="OPTANG";;
-	esac
-done
 
 git init
 git add .
 git commit -m "Init Commit"
 git branch -M main
-git remote add origin $repo
+git remote add origin $1
 git push -u origin main
